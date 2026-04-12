0d07c69ecabe0e8782c93f2410740ae2e5f71ad6
thanjanbaburaj <thanjanbaburaj@gmail.com>
2026-04-13 00:02:55 +0400

    Update config.py

--- shared/config.py
+++ shared/config.py
@@ -1,1 +1,14 @@
+import os
+import streamlit as st
 
+def get_apps_script_base_url() -> str:
+    return st.secrets["backend"]["apps_script_base_url"]
+
+def get_sheet_id() -> str:
+    return st.secrets["backend"]["sheet_id"]
+
+def get_console_password() -> str:
+    return st.secrets["auth"]["console_password"]
+
+def get_clientview_password() -> str:
+    return st.secrets["auth"]["clientview_password"]
